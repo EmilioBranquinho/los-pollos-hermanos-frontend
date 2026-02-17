b207b520bc94be2e12c31a37a7849697dd80e664
Emílio Marcelino Honório Branquinho <emiliomarcelino400@gmail.com>
2026-02-17 10:57:38 +0000

    feat: added the logo to the Header and improved the UX/UI of the rest of pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 const ToastContainer = dynamic(
   () => import("react-toastify").then(m => m.ToastContainer),
@@ -14,6 +15,10 @@ export default function App({ Component, pageProps }: AppProps) {
 
   const noBgRoutes: Array<string> = ["/", "/signup"];
   const removeBg: boolean = noBgRoutes.includes(router.pathname);
+
+  <Head>
+    <meta property="og:image" content="https://los-pollos-hermanos-six.vercel.app/_next/static/media/logo.b308968a.png" />
+  </Head>
 
   return (
     <AuthProvider>
